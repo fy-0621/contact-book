0e4e4e77e1d08bd440c4501e422457f12cf7565f
fy-0621 <60965937+fy-0621@users.noreply.github.com>
2020-08-03 20:17:48 +0900

    device削除
    device削除

--- db/schema.rb
+++ db/schema.rb
@@ -12,19 +12,4 @@
 
 ActiveRecord::Schema.define(version: 20200708095421) do
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",                                null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.integer  "employee_code",                       null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["name"], name: "index_users_on_name", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  end
-
 end
